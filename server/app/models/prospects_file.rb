aa19129de32196898dacb67dca1f0436778b9773
sbasher314 <sbasher314@gmail.com>
2022-02-04 22:56:31 -0500

    Made max_rows err msg dynamic, seperated row counting and prosepcts importing methods

--- server/app/models/prospects_file.rb
+++ server/app/models/prospects_file.rb
@@ -1,24 +1,30 @@
 class ProspectsFile < ApplicationRecord
   validates_presence_of :file, :email_index, :user_id
-  after_commit :count_rows, on: :create
+  after_commit :save_row_count, :on_create, on: :create
   has_one_attached :file
 end
 
 private
-  def count_rows
-    max_rows = 1000000
+  @@max_rows = 1000000
+  @@filepath = ''
+
+  def save_row_count
     rows = 0
-    filepath = ActiveStorage::Blob.service.path_for(self.file.key)
-    CSV.foreach(filepath, headers: self.has_headers, col_sep: ',') do |row|
+    @@filepath = ActiveStorage::Blob.service.path_for(self.file.key)
+    CSV.foreach(@@filepath, headers: self.has_headers, col_sep: ',') do |row|
       rows += 1
-      break if rows > max_rows
+      break if rows > @@max_rows
     end
     update! row_count: rows
-    if row_count < max_rows
-      ImportProspectsJob.perform_later(filepath, self)
+  end
+
+  def on_create
+    if row_count < @@max_rows
+      ImportProspectsJob.perform_later(@@filepath, self)
     else
       self.file.purge
       self.destroy
-      raise 'File contains too many rows (Limit 1,000,000)'
+      raise 'File contains too many rows (Limit '+ ActiveSupport::NumberHelper.number_to_delimited(max_rows).to_s + ')'
     end
   end
+
